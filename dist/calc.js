62e35a0079800f966f9e4c76bb933efa078c2460
Sudarshan Rai <sudarshan@yourgotocoder.com>
2023-05-19 21:43:34 +0530

    Setup for testing

---
+++ dist/calc.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = void 0;
+function add(value1, value2) {
+    return value1 + value2;
+}
+exports.add = add;
---
+++ src/calc.ts
@@ -0,0 +1,3 @@
+export function add(value1: number, value2: number): number {
+  return value1 + value2;
+}
