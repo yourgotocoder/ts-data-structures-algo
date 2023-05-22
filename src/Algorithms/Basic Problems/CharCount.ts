f441d66eb2656129218ccf379f4e03b37ba07794
Sudarshan Rai <sudarshan@yourgotocoder.com>
2023-05-22 22:22:10 +0530

    Improved version of char count

--- src/Algorithms/Basic Problems/CharCount.ts
+++ src/Algorithms/Basic Problems/CharCount.ts
@@ -1,11 +1,9 @@
 export function CharCount(sentence: string): { [key: string]: number } {
   const resultObj: { [key: string]: number } = {};
   for (let char of sentence.toLowerCase()) {
-    if (char === "" || char === " ") continue;
-    if (!resultObj[char]) {
-      resultObj[char] = 1;
-    } else if (resultObj[char]) {
-      resultObj[char]++;
+    const charCode: number = char.charCodeAt(0);
+    if ((charCode > 47 && charCode < 58) || (charCode > 96 && charCode < 123)) {
+      resultObj[char] = ++resultObj[char] || 1;
     }
   }
   return resultObj;
