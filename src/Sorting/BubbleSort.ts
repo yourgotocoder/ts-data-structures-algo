740925fc8e1c9cb36a0e00ba5b81e8948f218664
Sudarshan Rai <yourgotocoder@gmail.com>
2023-05-29 11:24:01 +0530

    Optimized Bubble Sort

--- src/Sorting/BubbleSort.ts
+++ src/Sorting/BubbleSort.ts
@@ -4,10 +4,21 @@ export function BubbleSort(arr: number[]): number[] {
     arr[index1] = arr[index2];
     arr[index2] = temp;
   };
+  let noSwaps = true;
+  let numberOfComparisions = 0;
   for (let outerIndex = arr.length - 1; outerIndex > 0; outerIndex--) {
+    noSwaps = true;
     for (let innerIndex = 0; innerIndex < outerIndex; innerIndex++) {
-      arr[innerIndex] > arr[innerIndex + 1] && swap(innerIndex, innerIndex + 1);
+      if (arr[innerIndex] > arr[innerIndex + 1]) {
+        swap(innerIndex, innerIndex + 1);
+        noSwaps = false;
+      }
+      ++numberOfComparisions;
     }
+    if (noSwaps) break;
   }
+  console.log(numberOfComparisions);
   return arr;
 }
+
+// [ 28,  18, 20, 3,45, 49]
--- test/BubbleSort.test.ts
+++ test/BubbleSort.test.ts
@@ -2,6 +2,6 @@ import { BubbleSort } from "../src/Sorting/BubbleSort";
 
 describe("takes an array of number and sorts it in the ascending order", () => {
   it("BubbleSort([8, 9, 2, 1, 5]) will return [1, 2, 5, 8, 9]", () => {
-    expect(BubbleSort([8, 9, 2, 1, 5])).toStrictEqual([1, 2, 5, 8, 9]);
+    expect(BubbleSort([2, 1, 5, 8, 9])).toStrictEqual([1, 2, 5, 8, 9]);
   });
 });
