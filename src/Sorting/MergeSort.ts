303ce67223aeb5ff92575d25ef427613780b5f76
Sudarshan Rai <yourgotocoder@gmail.com>
2023-06-07 16:31:29 +0530

    A more readable merge function

--- src/Sorting/MergeSort.ts
+++ src/Sorting/MergeSort.ts
@@ -1,30 +1,26 @@
 export function merge(arr1: number[], arr2: number[]): number[] {
-  const newArray: number[] = new Array(arr1.length + arr2.length);
+  const newArray: number[] = [];
   let indexOfArr1: number = 0;
   let indexOfArr2: number = 0;
-  let indexOfNewArray: number = 0;
-  while (indexOfNewArray < newArray.length) {
-    if (indexOfArr1 < arr1.length && indexOfArr2 < arr2.length) {
-      if (arr1[indexOfArr1] > arr2[indexOfArr2]) {
-        newArray[indexOfNewArray] = arr2[indexOfArr2];
-        indexOfArr2++;
-      } else if (arr1[indexOfArr1] < arr2[indexOfArr2]) {
-        newArray[indexOfNewArray] = arr1[indexOfArr1];
-        indexOfArr1++;
-      } else {
-        newArray[indexOfNewArray] = arr1[indexOfArr1];
-        indexOfArr1++;
-      }
-    } else if (indexOfArr1 >= arr1.length && indexOfArr2 < arr2.length) {
-      newArray[indexOfNewArray] = arr2[indexOfArr2];
-      indexOfArr2++;
-    } else {
-      newArray[indexOfNewArray] = arr1[indexOfArr1];
+  while (indexOfArr1 < arr1.length && indexOfArr2 < arr2.length) {
+    if (arr1[indexOfArr1] <= arr2[indexOfArr2]) {
+      newArray.push(arr1[indexOfArr1]);
       indexOfArr1++;
+    } else {
+      newArray.push(arr2[indexOfArr2]);
+      indexOfArr2++;
     }
-    indexOfNewArray++;
   }
+  while (indexOfArr1 < arr1.length) {
+    newArray.push(arr1[indexOfArr1]);
+    indexOfArr1++;
+  }
+  while (indexOfArr2 < arr2.length) {
+    newArray.push(arr2[indexOfArr2]);
+    indexOfArr2++;
+  }
+
   return newArray;
 }
 
-console.log(merge([1, 2, 3], [4, 5, 6, 7]));
+console.log(merge([1, 2, 3], [3, 5, 6]));
