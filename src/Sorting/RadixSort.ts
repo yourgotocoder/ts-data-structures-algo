26d5e13dfcbb61fe18a75a0a52c6a1e3b3bfa29a
Sudarshan Rai <sudarshan@yourgotocoder.com>
2023-06-08 22:29:50 +0530

    Radix sort, my solution

--- src/Sorting/RadixSort.ts
+++ src/Sorting/RadixSort.ts
@@ -1,4 +1,29 @@
-export function RadixSort() {}
+export function RadixSort(numArray: number[]): number[] {
+  const maxDigits = mostDigits(numArray);
+  for (let index = 0; index < maxDigits; index++) {
+    let bucket: { [key: number]: number[] } = {
+      0: [],
+      1: [],
+      2: [],
+      3: [],
+      4: [],
+      5: [],
+      6: [],
+      7: [],
+      8: [],
+      9: [],
+    };
+    for (let num of numArray) {
+      const digit = getDigit(num, index);
+      bucket[digit].push(num);
+    }
+    numArray = [];
+    for (let key in bucket) {
+      numArray.push(...bucket[key]);
+    }
+  }
+  return numArray;
+}
 
 function getDigit(num: number, place: number): number {
   return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
@@ -13,10 +38,12 @@ function digitCount(num: number): number {
 }
 
 function mostDigits(numArr: number[]): number {
-  let maxDigits = 0;
-  for (let num of numArr) {
-    if (digitCount(num) > maxDigits) maxDigits = digitCount(num);
-  }
+  let maxDigits = numArr.reduce(
+    (prevValue: number, currentValue: number) =>
+      Math.max(prevValue, digitCount(currentValue)),
+    0
+  );
   return maxDigits;
 }
-console.log(mostDigits([32, 123, 2, 3234, 23245, 123125]));
+
+console.log(RadixSort([23, 123, 12, 123213, 11555, 47457]));
