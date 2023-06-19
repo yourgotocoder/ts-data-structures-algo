9ce6bc3df79baf6f51c99a533c2096251a53a16d
Sudarshan Rai <sudarshan@yourgotocoder.com>
2023-06-19 11:21:14 +0530

    Hash using prime numbers

--- src/DataStructures/HashTables/HashTable.ts
+++ src/DataStructures/HashTables/HashTable.ts
@@ -8,21 +8,25 @@ class HashTable<T> {
 
   hash(key: string): number {
     let total = 0;
-    for (let char of key) {
-      total = (total + (char.charCodeAt(0) - 96)) % this.size;
+    const prime = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      total = (total * prime + (char.charCodeAt(0) - 96)) % this.size;
     }
     return total;
   }
   add(key: string, value: T): void {
     this.store[this.hash(key)] = value;
   }
-  get(key: string): T {
-    return this.store[this.hash(key)];
+  get(key: string): T | null {
+    return this.store[this.hash(key)] || null;
   }
 }
 
 const table = new HashTable(100);
 
 table.add("key", 1);
+table.add("zion", "zie");
 
 console.log(table.get("key"));
+console.log(table.get("zion"));
