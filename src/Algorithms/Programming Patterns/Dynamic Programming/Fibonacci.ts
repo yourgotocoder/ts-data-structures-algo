2648637c4b22811f675358a9e347ce0c826e290e
Sudarshan Rai <sudarshan@yourgotocoder.com>
2023-06-23 11:54:57 +0530

    Fib using bottom up approach

--- src/Algorithms/Programming Patterns/Dynamic Programming/Fibonacci.ts
+++ src/Algorithms/Programming Patterns/Dynamic Programming/Fibonacci.ts
@@ -1,11 +1,17 @@
-export function Fibonacci(n: number, cache: number[]): number {
-  if (cache[n] !== undefined) return cache[n];
+export function Fibonacci(n: number, cache: number[] = []): number {
+  // if (cache[n] !== undefined) return cache[n];
+  // if (n <= 2) return 1;
+  // let result = Fibonacci(n - 1, cache) + Fibonacci(n - 2, cache);
+  // cache[n] = result;
+  // return result;
   if (n <= 2) return 1;
-  let result = Fibonacci(n - 1, cache) + Fibonacci(n - 2, cache);
-  cache[n] = result;
-  return result;
+  let fibCache: number[] = [0, 1, 1];
+  for (let cacheIndex = 3; cacheIndex <= n; cacheIndex++) {
+    fibCache[cacheIndex] = fibCache[cacheIndex - 1] + fibCache[cacheIndex - 2];
+  }
+  return fibCache[n];
 }
 const start = performance.now();
 
-console.log(Fibonacci(45, []));
+console.log(Fibonacci(10000));
 console.log(`Time taken: ${(performance.now() - start) / 1000} sec`);
